7b05cb95b56df8630d2f6fdd793cc24a7e21dbea
Lukas Zahnd <luk@exside.ch>
2015-12-09 21:35:04 +0100

    Fix typo in en lexicon and correct some sentences

--- core/components/admintools/lexicon/en/default.inc.php
+++ core/components/admintools/lexicon/en/default.inc.php
@@ -3,7 +3,7 @@
 $_lang['admintools'] = 'AdminTools';
 $_lang['admintools_show_favorites'] = 'Show favorites';
 $_lang['admintools_show_all'] = 'Show all';
-$_lang['admintools_add_to_favorites'] = 'Add to faborites';
+$_lang['admintools_add_to_favorites'] = 'Add to favorites';
 $_lang['admintools_remove_from_favorites'] = 'Remove from favorites';
 
 $_lang['admintools_open'] = 'Go to element page';
@@ -11,9 +11,9 @@ $_lang['admintools_close'] = 'Close';
 $_lang['admintools_clear'] = 'Remove all';
 $_lang['admintools_last_edited'] = 'Recently edited elements';
 $_lang['admintools_item_remove'] = 'Remove row';
-$_lang['admintools_items_remove'] = 'Remove the rows';
-$_lang['admintools_item_remove_confirm'] = 'Are you sure you want to delete a row?';
-$_lang['admintools_item_remove_all_confirm'] = 'Are you sure you want to delete all records?';
+$_lang['admintools_items_remove'] = 'Remove rows';
+$_lang['admintools_item_remove_confirm'] = 'Are you sure you want to delete this row?';
+$_lang['admintools_item_remove_all_confirm'] = 'Are you sure you want to delete all rows?';
 $_lang['admintools_name'] = 'Name';
 $_lang['admintools_type'] = 'Element type';
 $_lang['admintools_date'] = 'Changed on';
