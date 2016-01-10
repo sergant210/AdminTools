c2d1d0437204972a8db82bf7272e4479677e0638
Sergey Shlokov <sergant210@bk.ru>
2016-01-10 20:20:15 +0300

    Delete properties.inc.php

--- core/components/admintools/lexicon/de/properties.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-$_lang['admintools_prop_limit'] = 'The number of Items to limit per page.';
-$_lang['admintools_prop_outputSeparator'] = 'A string to separate each row with.';
-$_lang['admintools_prop_sortBy'] = 'The field to sort by.';
-$_lang['admintools_prop_sortDir'] = 'The direction to sort by.';
-$_lang['admintools_prop_tpl'] = 'The chunk to use for each row of Items.';
-$_lang['admintools_prop_toPlaceholder'] = 'If set, will output the content to the placeholder specified in this property, rather than outputting the content directly.';
-*/
